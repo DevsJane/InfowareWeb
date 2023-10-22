525b52f8bfaf787d51e1b307327965d0bc6953c1
Nicolas Contreras <nmj.contreras@gmail.com>
2023-10-21 23:55:03 -0300

    Implementacion basica de la funcion Insertar de php a base de datos SQL

---
+++ insertar.php
@@ -0,0 +1,16 @@
+<?php
+include("index.php");
+
+$username = "jane";
+$password = "1110";
+$nombre = "Nicolas";
+$apellido = "Contreras";
+$telefono = "972061250";
+$sexo = "Masculino";
+$fecha_nacimiento = "2001/01/01";
+$descripcion = "Ola, esto es una prueba xd";
+$direccion = "Mi casa";
+
+mysqli_query($conexion,"INSERT INTO usuarios(username,password,nombre,apellido,telefono,sexo,fecha_nacimiento,descripcion,direccion) 
+            VALUES('$username','$password','$nombre','$apellido','$telefono','$sexo','$fecha_nacimiento','$descripcion','$direccion')")
+?>
