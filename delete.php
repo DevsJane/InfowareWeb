4b9c0229b5e3dc79d0504a1e273623ede5e3a64a
Nicolas Contreras <nmj.contreras@gmail.com>
2023-10-24 23:35:14 -0300

    Implementancion basica de la funcion Delete con php en el CRUD grafico

---
+++ delete.php
@@ -0,0 +1,14 @@
+<?php
+
+include("conexion.php");
+
+$username=$_GET['id'];
+
+$sql="DELETE FROM usuarios  WHERE username='$username'";
+$query=mysqli_query($conexion,$sql);
+
+if($query){
+    Header("Location: indexAdmin.php");
+}
+
+?>
