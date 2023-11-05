ab8bdcb690e0ab1308e8c486f3106061e7daf68e
Nicolas Contreras <nmj.contreras@gmail.com>
2023-11-05 17:54:18 -0300

    Implementacion Busqueda Avanzada en Modal

--- IndexAdmin.php
+++ IndexAdmin.php
@@ -197,7 +197,7 @@
                     
                     <div class="col-md-3 mx-auto">
                     <button class="btn btn-primary d-flex mx-auto mb-5" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">Insertar Usuario</button>
-                    <div class="offcanvas offcanvas-top bg-dark text-white" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel" style="height:500px;">
+                    <div class="offcanvas offcanvas-top bg-dark text-white" data-bs-backdrop="static" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel" style="height:500px;">
                       <div class="offcanvas-header">
                         <h5 class="offcanvas-title" id="offcanvasTopLabel">Insertar Usuario</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
@@ -272,13 +272,195 @@
                         </form>
                       </div>
                     </div>
-                            
-                        
+
+                    <div class="col-md-3 mx-auto">
+                      <!-- Botón para abrir el modal -->
+                      <button type="button" class="btn btn-primary d-flex mx-auto mb-5" data-toggle="modal" data-target="#miModal">
+                        Busqueda Avanzada
+                      </button>
+                    </div>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="miModal" tabindex="-1" role="dialog" aria-labelledby="miModalLabel" aria-hidden="true">
+                      <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
+                        <div class="modal-content">
+                          <div class="modal-header bg-dark">
+                            <h5 class="modal-title" id="miModalLabel">Busqueda Avanzada</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+                          </div>
+                          <div class="modal-body bg-dark">
+                            <!--Busqueda Avanzada-->
+                            <div class="container py-4 text-center d-flex flex-column align-items-center justify-content-center">
+                            <h2>Usuarios</h2>
+
+                            <div class="row g-4">
+
+                                <div class="col-auto">
+                                    <label for="num_registros" class="col-form-label">Mostrar: </label>
+                                </div>
+
+                                <div class="col-auto">
+                                    <select name="num_registros" id="num_registros" class="form-select">
+                                        <option value="10">10</option>
+                                        <option value="25">25</option>
+                                        <option value="50">50</option>
+                                        <option value="100">100</option>
+                                    </select>
+                                </div>
+
+                                <div class="col-auto">
+                                    <label for="num_registros" class="col-form-label">registros </label>
+                                </div>
+
+                                <div class="col-5"></div>
+
+                                <div class="col-auto">
+                                    <label for="campo" class="col-form-label">Buscar: </label>
+                                </div>
+                                <div class="col-auto">
+                                    <input type="text" name="campo" id="campo" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="row py-4">
+                                <div class="col">
+                                    <table class="table table-sm table-bordered table-striped divTabla">
+                                        <thead>
+                                            <th class="sort asc" style="font-size:10px; width:20px;">id</th>
+                                            <th class="sort asc" style="font-size:10px; width:20px;">email</th>
+                                            <th class="sort asc" style="font-size:10px; width:20px;">username</th>
+                                            <th class="sort asc" style="font-size:10px; width:20px;">password</th>
+                                            <th class="sort asc" style="font-size:10px; width:20px;">nombre</th>
+                                            <th class="sort asc" style="font-size:10px; width:20px;">apellido</th>
+                                            <th class="sort asc" style="font-size:10px; width:20px;">sexo</th>
+                                            <th class="sort asc" style="font-size:10px; width:20px;">fecha_nacimiento</th>
+                                            <th class="sort asc" style="font-size:10px; width:20px;">pais</th>
+                                            <th class="sort asc" style="font-size:10px; width:20px;">area_interes</th>
+                                            <th style="font-size:10px; width:20px;"></th>
+                                            <th style="font-size:10px; width:20px;"></th>
+                                        </thead>
+
+                                        <!-- El id del cuerpo de la tabla. -->
+                                        <tbody id="content">
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-6">
+                                    <label id="lbl-total"></label>
+                                </div>
+
+                                <div class="col-6" id="nav-paginacion"></div>
+
+                                <input type="hidden" id="pagina" value="1">
+                                <input type="hidden" id="orderCol" value="0">
+                                <input type="hidden" id="orderType" value="asc">
+                            </div>
+                        </div>
+
+                        <script>
+                        /* Llamando a la función getData() */
+                        getData()
+
+                        /* Escuchar un evento keyup en el campo de entrada y luego llamar a la función getData. */
+                        document.getElementById("campo").addEventListener("keyup", function() {
+                            getData()
+                        }, false)
+                        document.getElementById("num_registros").addEventListener("change", function() {
+                            getData()
+                        }, false)
+
+
+                        /* Peticion AJAX */
+                        function getData() {
+                            let input = document.getElementById("campo").value
+                            let num_registros = document.getElementById("num_registros").value
+                            let content = document.getElementById("content")
+                            let pagina = document.getElementById("pagina").value
+                            let orderCol = document.getElementById("orderCol").value
+                            let orderType = document.getElementById("orderType").value
+
+                            if (pagina == null) {
+                                pagina = 1
+                            }
+
+                            let url = "load.php"
+                            let formaData = new FormData()
+                            formaData.append('campo', input)
+                            formaData.append('registros', num_registros)
+                            formaData.append('pagina', pagina)
+                            formaData.append('orderCol', orderCol)
+                            formaData.append('orderType', orderType)
+
+                            fetch(url, {
+                                    method: "POST",
+                                    body: formaData
+                                }).then(response => response.json())
+                                .then(data => {
+                                    content.innerHTML = data.data
+                                    document.getElementById("lbl-total").innerHTML = 'Mostrando ' + data.totalFiltro +
+                                        ' de ' + data.totalRegistros + ' registros'
+                                    document.getElementById("nav-paginacion").innerHTML = data.paginacion
+                                }).catch(err => console.log(err))
+                        }
+
+                        function nextPage(pagina){
+                            document.getElementById('pagina').value = pagina
+                            getData()
+                        }
+
+                        let columns = document.getElementsByClassName("sort")
+                        let tamanio = columns.length
+                        for(let i = 0; i < tamanio; i++){
+                            columns[i].addEventListener("click", ordenar)
+                        }
+
+                        function ordenar(e){
+                            let elemento = e.target
+
+                            document.getElementById('orderCol').value = elemento.cellIndex
+
+                            if(elemento.classList.contains("asc")){
+                                document.getElementById("orderType").value = "asc"
+                                elemento.classList.remove("asc")
+                                elemento.classList.add("desc")
+                            } else {
+                                document.getElementById("orderType").value = "desc"
+                                elemento.classList.remove("desc")
+                                elemento.classList.add("asc")
+                            }
+
+                            getData()
+                        }
+
+                        </script>
+
+                        <!-- Bootstrap core JS -->
+                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+                        <!---->
+
+                            </div>
+                          </div>
+                          
+                        </div>
+                      </div>
+                    </div>
+
+                    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+                    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+                    
 
                     </div>
 
-                    <div class="col-md-8 d-flex mx-5">
-                        <table class="table table-dark" >
+                    <div class="col-md-8 d-flex mr-5">
+                        <table class="table table-dark" style="width:200px;height:200px;">
                             <thead class="table-success table-striped table-light" >
                                 <tr>
                                     <th style="text-align: center;padding: 15px;">Email</th>
